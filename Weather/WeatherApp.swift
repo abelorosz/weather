c9ff6c66723711208f72182dc5f59f888e566e41
Abel Orosz <abel@abelorosz.com>
2022-12-16 23:01:09 +0100

    Initial commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Abel Orosz on 2022. 12. 16..
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
